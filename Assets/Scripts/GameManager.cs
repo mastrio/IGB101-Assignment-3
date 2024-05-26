2bf290851605c5d84bf2589ad55785e106f28175
ThePinkOtter <SomePinkOtter@gmail.com>
2024-05-26 14:46:36 +1000

    pain

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -19,7 +19,11 @@ public class GameManager : MonoBehaviour
 
     void Start()
     {
-        
+        //Preloads audio to prevent lag, mostly for my Claire de Lune music clip - Brian
+        foreach (AudioSource source in audioSources)
+        {
+            source.clip.LoadAudioData();
+        }
     }
 
     void Update()
